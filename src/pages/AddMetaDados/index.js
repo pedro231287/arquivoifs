c12166450388489a5609997daa7d6747eb29f00a
Pedro Henrique Albuquerque Franca <pedro.franca@ifs.edu.br>
2025-04-01 15:24:40 -0300

    Salvando alterações antes do pull

--- src/pages/AddMetaDados/index.js
+++ src/pages/AddMetaDados/index.js
@@ -122,7 +122,6 @@ const AddMetaDados = () => {
     <html>
       <head>
         <title>ADCIONAR metadados</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
       </head>
       <body>
 
